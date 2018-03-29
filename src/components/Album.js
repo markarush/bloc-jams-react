c6264f43b0ecff10b5ebff57ceb5994b6c946076
Marka Rush <markabodine@gmail.com>
2018-03-28 22:26:44 -0500

    Components:Library complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Landing = () => (
-  <section className="album">
-    Album page will go here
-  </section>
-);
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
-export default Landing;
+export default Album;
